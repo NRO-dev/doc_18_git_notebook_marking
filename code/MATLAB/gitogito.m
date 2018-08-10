a004b45f531aca8cc4eef2d0f83e2c844bcd0719
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2018-08-10 12:15:27 +0200

    Changing folder levels

--- code/MATLAB/gitogito.m
+++ code/MATLAB/gitogito.m
@@ -1,6 +1,9 @@
 function gitogito()
 
     disp('Hello User,')
+    disp(' ')
+    disp('Git Hash Inquiry Algorithm')
+    disp('--------------------------------------------')
     disp(join(['Today is:              ', date]))
     disp(' ')
     
@@ -9,9 +12,17 @@ function gitogito()
     disp(join(['Java Version:          ', version('-java')]))
     disp(' ')
     
-    disp('Git Version:')
-    !git --version
+    %disp('Git Version:')
+    %!git --version
     
+    %read current directory
+    tempDir = pwd
+    
+    %test if current directory is git repo (.git existence)
+    %if not, go up to parent
+    
+    %if it worked display hash and return to tempDir
+    %if not, print error message that you probably aren't in a git repo
     disp(' ')
     disp('Hash Number:')
     !git rev-parse HEAD
