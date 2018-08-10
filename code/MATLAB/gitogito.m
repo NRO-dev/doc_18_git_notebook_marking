323108929d3592c84413ff9e8bfdb7ef993605d4
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2018-08-10 15:19:58 +0200

    Remove redundant words

--- code/MATLAB/gitogito.m
+++ code/MATLAB/gitogito.m
@@ -1,28 +1,16 @@
 function gitogito()
 
-    disp('Hello User,')
-    disp(' ')
-    disp('Git Hash Inquiry Algorithm')
-    disp('--------------------------------------------')
-    disp(join(['Today is:              ', date]))
+    disp('Notebook Tracking and Git Repository Information')
+    disp('------------------------------------------------------')
+    disp(join(['Inquiry date:          ', date]))
     disp(' ')
     
     disp(join(['Matlab Version:        ', version]))
     disp(join(['Matlab Version Date:   ', version('-date')]))
     disp(join(['Java Version:          ', version('-java')]))
     disp(' ')
-    
-    %disp('Git Version:')
-    %!git --version
-    
-    %read current directory
-    tempDir = pwd
-    
-    %test if current directory is git repo (.git existence)
-    %if not, go up to parent
-    
-    %if it worked display hash and return to tempDir
-    %if not, print error message that you probably aren't in a git repo
+    disp(['Current Directory:'])
+    disp(pwd)
     disp(' ')
     disp('Hash Number:')
     !git rev-parse HEAD
